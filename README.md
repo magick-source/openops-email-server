e5ea6316ebf51f15119d3bcb7df7ac6d368f7dcb
theMage <themage@magick-source.net>
2020-10-07 16:09:48 +0200

    initial files, nothing here yet
    themage

---
+++ README.md
@@ -0,0 +1,2 @@
+# Install a debian mail server from scratch
+
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+# ----------------------------------
+# Colors
+# ----------------------------------
+NOCOLOR='\033[0m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+BLUE='\033[1;34m'
+RED='\033[0;31m'
+
+
+# checking if we are in a debian
+if [ ! -f /etc/debian_version ]; then
+
+  echo "${RED}ERROR: This install file was created for debian$NOCOLOR"
+  echo "$YELLOW  * this doesn't seem to be running in debian.$NOCOLOR"
+  echo "$YELLOW  Bailing out!!$NOCOLOR"
+
+  exit 1;
+fi
+
+$debian_version = $( cat /etc/debian_version )
+
+echo "$BLUE >> Running in Debian $debian_version$NOCOLOR"
+
+
